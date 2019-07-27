7a28406686d2240d75713e0efb61d7708c78538f
petrov <gultesurta@desoz.com>
2019-07-28 01:00:07 +0300

    add

---
+++ 2/js/index-page.js
@@ -0,0 +1,218 @@
+$(function() {
+	// wow js
+	// new WOW().init();
+
+
+
+	// slick slider
+	$('.slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		adaptiveHeight: false,
+		arrows: true,
+		dots: false,
+		//swipe: false,
+		//swipeToSlide: false,
+		//touchMove: false,
+		//accessibility: false,
+		//centerMode: true,
+		//centerPadding: '60px',
+		autoplay: false,
+		autoplaySpeed: 3000,
+		prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="display: block;"></button>',
+		nextArrow: '<button class="slick-next slick-arrow" aria-label="Previous" type="button" style="display: block;"></button>',
+		responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+				arrows: true,
+				//centerMode: true,
+				//centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				arrows: true,
+				//centerMode: true,
+				//centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			}
+	  ]
+	});
+
+
+
+
+
+	// slick slider2
+	$('.slider2').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		adaptiveHeight: false,
+		arrows: true,
+		dots: false,
+		//swipe: false,
+		//swipeToSlide: false,
+		//touchMove: false,
+		//accessibility: false,
+		//centerMode: true,
+		//centerPadding: '60px',
+		autoplay: false,
+		autoplaySpeed: 3000,
+		prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style="display: block;"></button>',
+		nextArrow: '<button class="slick-next slick-arrow" aria-label="Previous" type="button" style="display: block;"></button>',
+		responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+				arrows: true,
+				//centerMode: true,
+				//centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				arrows: true,
+				//centerMode: true,
+				//centerPadding: '40px',
+				slidesToShow: 1
+			  }
+			}
+	  ]
+	});
+
+
+
+
+
+
+	
+
+
+
+
+
+	// popup
+	// ставим на кнопку которая вызывает данный popup
+	$('.js-popup').on('click', function(e) {
+	  e.preventDefault();
+
+	  // в input hidden помещаем значение button в data-user - когда нужно передать значение из button
+	  // <button class="btnbig js-popup" data-user="узнать о программе - блок: ДИСТАНЦИОННАЯ ПРОГРАММА">узнать о программе</button>
+	  // $('.popup .main-form input[name="user_data"]').val($(this).attr('data-user'));
+
+	  // поиск popup и popup-overlay для их открытия
+	  // $('.popup').css('display', 'block');
+	  // $('.popup-overlay').css('display', 'block');
+	  // or
+	  $('.popup').slideDown();
+	  $('.popup-overlay').css('display', 'block');
+	  
+	  // поиск button close popup
+	  $('.js-close-popup, .popup-overlay').on('click', function(e) {
+	  	e.preventDefault();
+			// $('.popup').css('display', 'none');
+			// $('.popup-overlay').css('display', 'none');
+			// or
+			$('.popup').slideUp('300', function() {
+				
+				// stop iframe video play after close popup
+				// var video = $(".popup iframe").attr("src");
+				// $(".popup iframe").attr("src", "");
+				// $(".popup iframe").attr("src", video);
+			});
+			$('.popup-overlay').css('display', 'none');
+			// $('.popup').find("input").val(""); // off in wp
+	  });
+	});
+
+	$('.js-popup2').on('click', function(e) {
+	  e.preventDefault();
+
+	  // в input hidden помещаем значение button в data-user - когда нужно передать значение из button
+	  // <button class="btnbig js-popup" data-user="узнать о программе - блок: ДИСТАНЦИОННАЯ ПРОГРАММА">узнать о программе</button>
+	  // $('.popup2 .main-form input[name="user_data"]').val($(this).attr('data-user'));
+
+	  // поиск popup и popup-overlay для их открытия
+	  // $('.popup').css('display', 'block');
+	  // $('.popup-overlay').css('display', 'block');
+	  // or
+	  $('.popup2').slideDown();
+	  $('.popup-overlay').css('display', 'block');
+	  
+	  // поиск button close popup
+	  $('.js-close-popup, .popup-overlay').on('click', function(e) {
+				e.preventDefault();
+			// $('.popup').css('display', 'none');
+			// $('.popup-overlay').css('display', 'none');
+			// or
+			$('.popup2').slideUp();
+			$('.popup-overlay').css('display', 'none');
+	  });
+	});
+
+
+
+	// tabs jquery
+	$(function() {
+		$(".tab_item").not(":first").hide();
+		$(".wrap .tab").click(function() {
+			
+			// нужно если slick+tab
+			//$('.slider').slick('slickGoTo', 0, false);
+			
+			$(".wrap .tab").removeClass("active").eq($(this).index()).addClass("active");
+			$(".tab_item").hide().eq($(this).index()).fadeIn()
+		}).eq(0).addClass("active");
+	});
+
+
+
+	// email
+	$('.main-form').submit(function(event) {
+		event.preventDefault();
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",	// url: "smart.php", даже если js и php не в одной папке!
+			data: $(this).serialize()
+		}).done(function(data) {
+			$(this).find("input").val("");
+			
+			//alert("Заявка отправлена!");
+			window.location.href = "../mailer/thanks.html";
+			
+			$("form").trigger("reset");
+			
+			$('.popup').slideUp();
+			$('.popup-overlay').css('display', 'none');
+		});
+		
+		// if (name !== '' & phone !== '') {
+		// 	$.ajax({
+		// 		type: "POST",
+		// 		url: "mailer/smart.php",
+		// 		data: $(this).serialize()
+		// 	}).done(function() {
+		// 		$(this).find("input").val("");
+				
+		// 		//alert("Заявка отправлена!");
+		// 		window.location.href = "../mailer/thanks.html";
+				
+		// 		$("form").trigger("reset");
+				
+		// 		$('.popup').slideUp();
+		// 		$('.popup-overlay').css('display', 'none');
+		// 	});
+		// } else {
+		// 	alert('Данные не заполнены!');
+		// }
+		
+		return false;
+	});
+});
